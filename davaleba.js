c88a0f723df64bfb3c44fc831f6454da45c82215
tatugrigalashvili <146704405+tatugrigalashvili@users.noreply.github.com>
2024-05-24 17:07:28 +0400

    Add files via upload

--- davaleba.js
+++ davaleba.js
@@ -1,4 +1,6 @@
-for(var masivi= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; masivi < 10; masivi++){
+var masivi= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for( masivi < 10; masivi++;){
     masivi.pop()
     document.write(masivi)
 }
